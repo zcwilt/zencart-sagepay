aefb9dddcc8603bf2273320067160ef1f6103f1a
zcwilt <ian@zen-cart.com>
2015-12-30 16:45:03 +0000

    updating docblocks

--- admin/includes/extra_datafiles/sagepay_transaction_database_tables.php
+++ admin/includes/extra_datafiles/sagepay_transaction_database_tables.php
@@ -1,25 +1,18 @@
 <?php
-// +----------------------------------------------------------------------+
-// | SagePay Form Payment Services Module          			              |
-// | 				       			        				              |
-// | admin/includes/extra_datafiles/sagepay_form_database_tables.php      |
-// | 				       			        				              |
-// | Released under GPL   							                      |
-// | 				       			        				              |
-// | Designed by Nixak								                      |
-// | Version 2.0 26th March 2011						                  |
-// +----------------------------------------------------------------------+
-// | $Id: sagepay_form v2.0 07944426031109222                             |
-// +----------------------------------------------------------------------+
- 
-// File protection as per Zen-Cart suggestions
-	if (!defined('IS_ADMIN_FLAG')) {
-	  die('Illegal Access');
-	}
-// EOF: File protection
-
-	if (!defined('DB_PREFIX')) {
-    define('DB_PREFIX', '');
-	}
-	define('TABLE_SAGEPAY_TRANSACTION', DB_PREFIX . 'sagepay_transaction');
+/**
+ * sagepay form
+ *
+ * @package paymentMethod
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Author: Wilt New in v1.5.5 $
+ */
+if (!defined('IS_ADMIN_FLAG')) {
+	die('Illegal Access');
+}
+if (!defined('DB_PREFIX')) {
+	define('DB_PREFIX', '');
+}
+define('TABLE_SAGEPAY_TRANSACTION', DB_PREFIX . 'sagepay_transaction');
 
