c17704755df31d092c058aa3338906fa2018d66e
zcwilt <ian@zen-cart.com>
2015-12-30 16:39:16 +0000

    updating docblocks

--- includes/modules/payment/sagepay_zc/AbstractSagepayAPI.php
+++ includes/modules/payment/sagepay_zc/AbstractSagepayAPI.php
@@ -1,10 +1,16 @@
 <?php
+/**
+ * sagepay form
+ *
+ * @package paymentMethod
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Author: Wilt New in v1.5.5 $
+ */
 
 /**
- * Created by PhpStorm.
- * User: wilt
- * Date: 18/11/15
- * Time: 17:43
+ * Class AbstractSagepayAPI
  */
 class AbstractSagepayAPI extends base
 {
--- includes/modules/payment/sagepay_zc/sagepay_form_admin_notification.php
+++ includes/modules/payment/sagepay_zc/sagepay_form_admin_notification.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * sagepay form
+ *
+ * @package paymentMethod
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: Author: Wilt New in v1.5.5 $
+ */
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
